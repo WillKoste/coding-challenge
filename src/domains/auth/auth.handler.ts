2bdaeb9eb76d66b584d78d7bea48cb2881663de6
Will Koste <willkoste@macbook.local>
2026-02-03 10:43:36 -0500

    Added trycatch to the login handler

--- src/domains/auth/auth.handler.ts
+++ src/domains/auth/auth.handler.ts
@@ -1,10 +1,15 @@
-import {Request, Response} from 'express';
-import {login} from './auth.service.js';
-import {LoginBody} from './auth.types.js';
+import { Request, Response } from "express";
+import { login } from "./auth.service.js";
+import { LoginBody } from "./auth.types.js";
 
 export const authLoginHandler = async (req: Request, res: Response) => {
-	const body: LoginBody = req.body;
-	const user = await login({...body, ip: req.ip});
+  try {
+    const body: LoginBody = req.body;
+    const user = await login({ ...body, ip: req.ip });
 
-	return res.status(200).json({success: true, data: user});
+    return res.status(200).json({ success: true, data: user });
+  } catch (err) {
+    console.error(`[authLoginHandler] Error: ${err}`);
+    return res.status(500).send("Server error");
+  }
 };
