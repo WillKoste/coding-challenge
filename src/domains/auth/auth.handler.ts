1ba9a67890a02a041059bbd6b6a1e83efbf9f4fa
Will Koste <willkoste@macbook.local>
2026-02-03 10:50:13 -0500

    Revert the trycatch - the thrown errors are more impactful for this particular instance

--- src/domains/auth/auth.handler.ts
+++ src/domains/auth/auth.handler.ts
@@ -3,13 +3,8 @@ import { login } from "./auth.service.js";
 import { LoginBody } from "./auth.types.js";
 
 export const authLoginHandler = async (req: Request, res: Response) => {
-  try {
-    const body: LoginBody = req.body;
-    const user = await login({ ...body, ip: req.ip });
+  const body: LoginBody = req.body;
+  const user = await login({ ...body, ip: req.ip });
 
-    return res.status(200).json({ success: true, data: user });
-  } catch (err) {
-    console.error(`[authLoginHandler] Error: ${err}`);
-    return res.status(500).send("Server error");
-  }
+  return res.status(200).json({ success: true, data: user });
 };
